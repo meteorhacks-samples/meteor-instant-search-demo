6802785038f5b8d99df20af0687930ee0cd2aaf7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-01-09 17:17:23 +0530

    upgraded search algorithm

--- server/app.js
+++ server/app.js
@@ -3,7 +3,11 @@ SearchSource.defineSource('packages', function(searchText, options) {
   
   if(searchText) {
     var regExp = buildRegExp(searchText);
-    var selector = {packageName: regExp, description: regExp};
+    var selector = {$or: [
+      {packageName: regExp},
+      {description: regExp}
+    ]};
+    
     return Packages.find(selector, options).fetch();
   } else {
     return Packages.find({}, options).fetch();
@@ -11,7 +15,7 @@ SearchSource.defineSource('packages', function(searchText, options) {
 });
 
 function buildRegExp(searchText) {
-  // this is dumb implementation
-  var parts = searchText.trim().split(' ');
+  // this is a dumb implementation
+  var parts = searchText.trim().split(/[ \-\:]+/);
   return new RegExp("(" + parts.join('|') + ")", "ig");
 }
